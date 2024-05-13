bdca9c5220b92ca96a74766577189e1ab716b164
JZITNIK-github <email@jzitnik.is-a.dev>
2024-05-13 18:22:11 +0200

    init

---
+++ backend/src/main/java/cz/jzitnik/quizappbackend/QuizappBackendApplication.java
@@ -0,0 +1,13 @@
+package cz.jzitnik.quizappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizappBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuizappBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/cz/jzitnik/quizappbackend/QuizappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.jzitnik.quizappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
