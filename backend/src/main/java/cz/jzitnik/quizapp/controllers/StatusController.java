e67df56aa13bcc9f8078eb626d097aab0b68a793
JZITNIK-github <email@jzitnik.dev>
2024-07-09 20:28:48 +0200

    chore: Added status mapping

---
+++ backend/src/main/java/cz/jzitnik/quizapp/controllers/StatusController.java
@@ -0,0 +1,14 @@
+package cz.jzitnik.quizapp.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/status")
+public class StatusController {
+    @GetMapping
+    public String status() {
+        return "running";
+    }
+}
