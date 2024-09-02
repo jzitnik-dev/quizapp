d09c93e9dcb542b0ec0d1154ab0d81c59b1a38b9
jzitnik <email@jzitnik.dev>
2024-09-02 13:42:58 +0000

    revert a324371da27033cf7200fc33072d554d12d937c9
    revert fix: Random quiz on different databases

--- backend/src/main/java/cz/jzitnik/quizapp/repository/CustomQuizRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.jzitnik.quizapp.repository;
-
-import cz.jzitnik.quizapp.entities.Quiz;
-
-import java.util.Optional;
-
-public interface CustomQuizRepository {
-    Optional<Quiz> findRandomQuiz();
-}
--- backend/src/main/java/cz/jzitnik/quizapp/repository/QuizRepository.java
+++ backend/src/main/java/cz/jzitnik/quizapp/repository/QuizRepository.java
@@ -8,12 +8,17 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.domain.Pageable;
 
+import java.util.Optional;
+
 
 @Repository
-public interface QuizRepository extends JpaRepository<Quiz, Long>, CustomQuizRepository {
+public interface QuizRepository extends JpaRepository<Quiz, Long> {
     @Query("SELECT q FROM Quiz q WHERE SIZE(q.questions) = ?1")
     Page<Quiz> findByNumberOfQuestions(int numberOfQuestions, Pageable pageable);
 
     @Query("SELECT q FROM Quiz q WHERE q.title LIKE %:keyword%")
     Page<Quiz> findByTitle(@Param("keyword") String keyword, Pageable pageable);
+
+    @Query(value = "SELECT q FROM Quiz q ORDER BY rand() LIMIT 1")
+    Optional<Quiz> findRandomQuiz();
 }
