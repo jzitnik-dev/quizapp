d36674cd8aace15ed07f3a2dba1f903d5d86d696
JZITNIK-github <email@jzitnik.dev>
2024-09-02 16:50:21 +0200

    fix: Cannot register

--- frontend/src/api/getRegisterAllowed.ts
+++ frontend/src/api/getRegisterAllowed.ts
@@ -3,5 +3,5 @@ import dataAxiosInstance from "./axios/dataAxiosInstance";
 export default async function getRegisterAllowed() {
   const response = await dataAxiosInstance.get("/config/registerAllowed");
 
-  return response.data == "true";
+  return response.data;
 }
