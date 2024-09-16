08a9eb499fdd65ed05caba3aa2e2cf534761ffec
JZITNIK-github <email@jzitnik.dev>
2024-09-16 15:50:20 +0200

    fix: Disable API when upload profile disabled

--- backend/src/main/java/cz/jzitnik/quizapp/controllers/ProfilePictureController.java
+++ backend/src/main/java/cz/jzitnik/quizapp/controllers/ProfilePictureController.java
@@ -1,10 +1,12 @@
 package cz.jzitnik.quizapp.controllers;
 
 import cz.jzitnik.quizapp.entities.User;
+import cz.jzitnik.quizapp.payload.response.MessageResponse;
 import cz.jzitnik.quizapp.repository.UserRepository;
 import cz.jzitnik.quizapp.services.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -29,9 +31,16 @@ public class ProfilePictureController {
     @Autowired
     private UserRepository userRepository;
 
+    @Value("${jzitnik.app.disableProfilePictureUpload}")
+    private String disablePPUpload;
+
     @PostMapping
     @PreAuthorize("isAuthenticated()")
     public ResponseEntity uploadImage(@RequestParam("file") MultipartFile file) throws IOException {
+        if (disablePPUpload.equals("true")) {
+            return ResponseEntity.badRequest().body(new MessageResponse("Nahrávání profilových obrázků je zakázano administrátorem!"));
+        }
+
         var user = userService.getCurrentUser();
 
         String filePath = "ProfilePictures/" + user.getId();
@@ -56,11 +65,11 @@ public class ProfilePictureController {
         String rootPath = System.getProperty("user.dir");
         Path imagePath = Paths.get(rootPath, "ProfilePictures", String.valueOf(user.getId()));
 
-            if (Files.exists(imagePath)) {
-                Files.copy(imagePath, response.getOutputStream());
-                response.getOutputStream().flush();
-            } else {
-                response.setStatus(HttpStatus.NOT_FOUND.value());
-            }
+        if (Files.exists(imagePath)) {
+            Files.copy(imagePath, response.getOutputStream());
+            response.getOutputStream().flush();
+        } else {
+            response.setStatus(HttpStatus.NOT_FOUND.value());
+        }
     }
 }
